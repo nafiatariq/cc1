eadd0547b247f13688ffef5301006e7a9bfc7f7b
Nafia Tariq <nafia.tariq2000@hotmail.com>
2021-09-28 19:33:01 -0400

    Made covid19 move (y) places

--- activities/04-dodging-covid-19/js/script.js
+++ activities/04-dodging-covid-19/js/script.js
@@ -1,27 +1,30 @@
 /**
-Title of Project
-Author Name
-
-This is a template. You must fill in the title,
-author, and this description to match your project!
+Activity 04: Dodging COVID-19
+Nafia Tariq
 */
 
-"use strict";
-
-
-/**
-Description of preload
-*/
-function preload() {
-
-}
-
+let covid19 = {
+  x: 0,
+  y: 250,
+  size: 100,
+  vx: 0,
+  vy: 0,
+  speed: 5,
+  fill: {
+    r: 255,
+    g: 0,
+    b: 0
+  }
+};
 
 /**
 Description of setup
 */
 function setup() {
+  createCanvas(windowWidth,windowHeight);
 
+  covid19.y = random(0,height);
+  covid19.vx = covid19.speed;
 }
 
 
@@ -29,5 +32,17 @@ function setup() {
 Description of draw()
 */
 function draw() {
+  background(0);
+
+  covid19.x = covid19.x + covid19.vx;
+  covid19.y = covid19.y + covid19.vy;
+
+  if(covid19.x > width){
+    covid19.x = 0;
+    covid19.y = random(0,height);
+  }
+
+  fill(covid19.fill.r,covid19.fill.g,covid19.fill.b);
+  ellipse(covid19.x,covid19.y,covid19.size);
 
 }
