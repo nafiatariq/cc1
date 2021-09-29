292a602c095f118bee3841d807a7656a6f5c5bc6
Nafia Tariq <nafia.tariq2000@hotmail.com>
2021-09-28 22:59:10 -0400

    Added covid19 image and fixed colours

--- exercises/dodge-em/js/script.js
+++ exercises/dodge-em/js/script.js
@@ -5,29 +5,24 @@ Nafia Tariq
 
 "use strict";
 let bg ={
-  r: 135,
-  g: 206,
-  b: 250
+  r: 163,
+  g: 193,
+  b: 173
 }
 
 let covid19 = {
   x: 0,
-  y: 250,
-  size: 100,
+  y: 200,
+  size: 200,
   vx: 0,
   vy: 0,
   speed: 5,
-  fill: {
-    r: 119,
-    g: 221,
-    b: 119
-  }
 };
 
 let user = {
   x: 250,
   y: 250,
-  size: 150,
+  size: 100,
   vx: 0,
   vy: 0,
   fill: {
@@ -39,6 +34,11 @@ let user = {
 
 let numStatic = 10;
 
+let covid19Image;
+
+function preload () {
+  covid19Image = loadImage("assets/images/virus.png");
+}
 
 function setup() {
   createCanvas(windowWidth,windowHeight);
@@ -53,25 +53,26 @@ function setup() {
 function draw() {
   background(bg.r,bg.g,bg.b);
 
+
 for (let i = 0; i < numStatic; i++){
   let x = random(0,width);
   let y = random(0,height);
-  stroke(79,134,247);
+  stroke(143,151,121);
   strokeWeight(10);
   point(x,y);
 }
 
   if (mouseX < covid19.x){
-    covid19.vx = -covid19.speed/2;
+    covid19.vx = -covid19.speed/4;
   }
   else {
-    covid19.vx = covid19.speed/2;
+    covid19.vx = covid19.speed/4;
   }
   if (mouseY < covid19.y){
-    covid19.vy = -covid19.speed/2;
+    covid19.vy = -covid19.speed/4;
   }
   else {
-    covid19.vy = covid19.speed/2;
+    covid19.vy = covid19.speed/4;
   }
 
   covid19.x = covid19.x + covid19.vx;
@@ -82,25 +83,24 @@ for (let i = 0; i < numStatic; i++){
     covid19.y = random(0,height);
   }
 
-  //user.x = mouseX;
-  //user.y = mouseY;
+  imageMode(CENTER);
+  image(covid19Image,covid19.x,covid19.y, covid19.size, covid19.size);
 
   let d = dist(user.x,user.y,covid19.x,covid19.y);
-  if (d < covid19.size/3 + user.size/3) {
+  if (d < covid19.size/4 + user.size/4) {
     noLoop();
   }
 
-  fill(covid19.fill.r,covid19.fill.g,covid19.fill.b);
-  strokeWeight(2);
-  rect(covid19.x,covid19.y,covid19.size);
+
 
   fill(user.fill.r,user.fill.g,user.fill.b);
   strokeWeight(2);
-  ellipse(user.x,user.y,user.size);
+  rect(user.x,user.y,user.size);
 
 }
 
 function mousePressed(){
   user.x = mouseX;
   user.y = mouseY;
+
 }
