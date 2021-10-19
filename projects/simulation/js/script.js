5624f21383b086680a5669ba9c6080642e9ee234
Nafia Tariq <nafia.tariq2000@hotmail.com>
2021-10-19 00:54:49 -0400

    P1: Loading in the images for the simulation

--- projects/simulation/js/script.js
+++ projects/simulation/js/script.js
@@ -5,3 +5,31 @@ Author Name
 This is a template. You must fill in the title,
 author, and this description to match your project!
 */
+
+"use strict";
+
+/**
+Description of preload
+*/
+
+let court;
+let tennisBall;
+
+function preload() {
+  court = loadImage("assets/images/court.jpg");
+  tennisBall = loadImage("assets/images/ball.png");
+}
+
+/**
+Description of setup
+*/
+function setup() {
+
+}
+
+/**
+Description of draw()
+*/
+function draw() {
+
+}
