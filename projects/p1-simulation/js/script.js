ca97cf73464716cda969091516a787dfb0ba180f
Nafia Tariq <nafia.tariq2000@hotmail.com>
2021-10-18 23:58:52 -0400

    P1: Started project 1

---
+++ projects/p1-simulation/js/script.js
@@ -0,0 +1,35 @@
+/**
+Project 1: Simulation
+Catch the Balls!
+
+Nafia Tariq
+
+Tennis balls are falling from the sky. Catch them with your
+white box! If you miss, you will have to start over.
+*/
+
+"use strict";
+
+
+/**
+Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+Description of setup
+*/
+function setup() {
+
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+
+}
