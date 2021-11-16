9c661fb917b1a693d8ba3e6aa0ddc18fc1174518
Nafia Tariq <nafia.tariq2000@hotmail.com>
2021-11-16 00:38:06 -0500

    Activity: Fixing html to make balls appear

--- activities/sound-activity/js/script.js
+++ activities/sound-activity/js/script.js
@@ -7,26 +7,39 @@ A program that plays music based on primitive physics.
 
 "use strict";
 
+// the balls
+let balls = [];
 
-/**
-Description of preload
-*/
-function preload() {
+// F-minor
+let notes = [`F3`, `G3`, `Ab4`, `Bb4`, `C4`, `Db4`, `Eb4`, `F4`];
 
-}
-
-
-/**
-Description of setup
-*/
+// create canvas
 function setup() {
+  createCanvas(600, 600);
 
+  userStartAudio();
 }
 
-
-/**
-Description of draw()
-*/
 function draw() {
 
+  background(0);
+
+  for (let i = 0; i < balls.length; i++) {
+    let ball = balls[i];
+    ball.move();
+    ball.bounce();
+    ball.display();
+  }
+}
+
+// press mouse to activate balls
+function mousePressed() {
+  let note = random(notes);
+  createBall(mouseX, mouseY, note);
+}
+
+// create ball
+function createBall(x, y) {
+  let ball = new Ball(x, y);
+  balls.push(ball);
 }
