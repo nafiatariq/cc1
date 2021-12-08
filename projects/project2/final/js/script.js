8d34bdc876e412199a4d8374b7a686ee8e5f7d98
Nafia Tariq <nafia.tariq2000@hotmail.com>
2021-12-07 20:45:43 -0500

    P2 Final: Description of the game

---
+++ projects/project2/final/js/script.js
@@ -0,0 +1,10 @@
+/**
+Project 2 - Prototype: City Escape
+Nafia Tariq
+
+Avoid buildings and birds by moving the airplane
+up and down with the keyboard arrows! The higher level you
+are at, the faster you will go.
+
+Fly Safely!
+*/
